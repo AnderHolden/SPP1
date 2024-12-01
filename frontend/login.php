e72e69a4188780d861514e9ba15287c6651f54dc
Ander Holden <urbanoivan10@gmail.com>
2024-12-01 10:41:51 -0500

    Se modifica el titulo de la pagina a "Diseña" en el archivo login.php en la linea 18

--- frontend/login.php
+++ frontend/login.php
@@ -7,7 +7,7 @@ include_once '../backend/php/login.php'
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Clínica | Salud</title>
+    <title>Diseña | Salud</title>
     <!-- Custom CSS -->
     <link rel="stylesheet" href="../backend/css/style.css" />
     <link rel="icon" type="image/png" sizes="96x96" href="../backend/img/ico.svg">
